d5745a975d47c00056a80b41d964d1a2b4ce731b
Bryce Kerley <bkerley@brycekerley.net>
2008-05-24 20:09:46 -0400

    perform the fetch

--- tdigester.rb
+++ tdigester.rb
@@ -1,3 +1,11 @@
 # http://twitter.com/statuses/user_timeline/bonzoesc.atom
 require 'rubygems'
 require 'twitter'
+require 'active_support'
+
+SINCE = 24.hours.ago
+USER = 'BonzoESC'
+
+c = Twitter::Client.new
+
+s = c.timeline_for :user, :id=>USER, :since=>SINCE
